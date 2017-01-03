2fd5820777e4e1d98878ea5deae4e22b9ef31532
Mike Erickson <codedungeon@gmail.com>
2017-01-03 12:30:22 -0800

    code update

--- src/messenger-node.js
+++ src/messenger-node.js
@@ -14,6 +14,7 @@ var messenger = {
   },
   log: function (...params) {
     console.log(...params);
+    return params;
   },
   info: function (...params) {
     console.log(chalk.cyan(...params));
--- test/node.spec.js
+++ test/node.spec.js
@@ -1,7 +1,32 @@
 let assert = require('chai').assert;
+let expect = require('chai').expect;
+
+let _      = require('lodash');
 
 describe('==> Messenger Node (Console)', () => {
+  let messenger;
+  let methods = ['version','info','note','log','error','success','warning','table','dir']
+  beforeEach(function () {
+    messenger = require('../src/messenger-node');
+  });
+
   it('should be instance of messenger-node', function () {
-    assert(true);
+    assert(_.isObject(messenger));
+  });
+
+  it('should contain log method', function () {
+    assert(messenger.hasOwnProperty('log'));
+  });
+
+  it('should contain all methods', function () {
+    methods.forEach((item) => {
+      assert(messenger.hasOwnProperty(item));
+    });
+  });
+  it('should return supplied message', function () {
+    var result = messenger.log('test message');
+    expect(result).to.be.array;
+    expect(result.length).to.equal(1);
+    expect(result[0]).to.equal('test message');
   });
 });
